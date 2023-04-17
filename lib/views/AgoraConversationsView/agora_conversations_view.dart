5c81869720ddc63d4a85259c849903f460ea273d
dujiepeng <347302029@qq.com>
2023-04-17 15:16:37 +0800

    update api referance.

--- lib/views/AgoraConversationsView/agora_conversations_view.dart
+++ lib/views/AgoraConversationsView/agora_conversations_view.dart
@@ -12,14 +12,19 @@ class AgoraConversationsController extends AgoraBaseController {
     loadAllConversations();
   }
 
-  AgoraConversationSortHandle? sortHandle;
   final ValueNotifier<List<ChatConversation>> _listValueNotifier =
       ValueNotifier([]);
+
   final ValueNotifier<int> _totalUnreadCountNotifier = ValueNotifier(0);
 
+  /// Conversation list sorting Handler.
+  AgoraConversationSortHandle? sortHandle;
+
+  /// Get conversation list.
   List<ChatConversation> get conversationList => _listValueNotifier.value;
   int get totalUnreadCount => _totalUnreadCountNotifier.value;
 
+  /// Set conversation list.
   set conversationList(List<ChatConversation> list) {
     _listValueNotifier.value = List.from(list);
 
@@ -28,18 +33,22 @@ class AgoraConversationsController extends AgoraBaseController {
         .then((value) => _totalUnreadCountNotifier.value = value);
   }
 
+  /// Registers the conversation list change event.
   void addListListener(VoidCallback function) {
     _listValueNotifier.addListener(function);
   }
 
+  /// Unregisters the conversation list change event.
   void removeListListener(VoidCallback function) {
     _listValueNotifier.removeListener(function);
   }
 
+  /// Registers the session read-out change event.
   void addTotalUnreadCountListener(VoidCallback function) {
     _totalUnreadCountNotifier.addListener(function);
   }
 
+  /// Unregisters the session read-out change event.
   void removeTotalUnreadCountListener(VoidCallback function) {
     _totalUnreadCountNotifier.removeListener(function);
   }
@@ -62,7 +71,7 @@ class AgoraConversationsController extends AgoraBaseController {
     ChatClient.getInstance.chatManager.removeEventHandler(key);
   }
 
-  /// LoadAllConversations
+  /// Update the conversations and refresh the list.
   Future<void> loadAllConversations() async {
     List<ChatConversation> list =
         await ChatClient.getInstance.chatManager.loadAllConversations();
@@ -70,6 +79,7 @@ class AgoraConversationsController extends AgoraBaseController {
   }
 
   /// Delete conversation with conversation id.
+  ///
   /// Param [id] conversation id
   Future<void> deleteConversationWithId(String id) async {
     List<ChatConversation> list = conversationList;
@@ -82,6 +92,8 @@ class AgoraConversationsController extends AgoraBaseController {
   }
 
   /// Delete all conversation.
+  ///
+  /// Param [includeMessage] Whether to delete messages at the same time.
   Future<void> deleteAllConversations({bool includeMessage = true}) async {
     List<ChatConversation> list = conversationList;
     await Future.wait(list
@@ -115,11 +127,53 @@ class AgoraConversationsController extends AgoraBaseController {
   }
 }
 
+/// Conversation list Widget
 class AgoraConversationsView extends StatefulWidget {
+  /// Conversation list Widget
+  /// [controller] The ScrollController for the conversation list.
+  ///
+  /// [onItemTap] Conversation list item Click event callback.
+  ///
+  /// [reverse] Creates a scrollable, linear array of widgets with a custom child model. For example, a custom child model
+  /// can control the algorithm used to estimate the size of children that are not actually visible.
+  ///
+  /// [primary] Creates a scrollable, linear array of widgets with a custom child model. For example, a custom child model can
+  /// control the algorithm used to estimate the size of children that are not actually visible.
+  ///
+  /// [physics] Creates a scrollable, linear array of widgets with a custom child model.
+  /// For example, a custom child model can control the algorithm used to estimate the size of children that are not actually visible.
+  ///
+  /// [shrinkWrap] Creates a scrollable, linear array of widgets with a custom child model. For example, a custom child model
+  /// can control the algorithm used to estimate the size of children that are not actually visible.
+  ///
+  /// [padding] Creates a scrollable, linear array of widgets with a custom child model. For example, a custom child model
+  /// can control the algorithm used to estimate the size of children that are not actually visible.
+  ///
+  /// [cacheExtent] Creates a scrollable, linear array of widgets with a custom child model. For example, a custom child model
+  /// can control the algorithm used to estimate the size of children that are not actually visible.
+  ///
+  /// [dragStartBehavior] Creates a scrollable, linear array of widgets with a custom child model.For example, a custom child model can
+  /// control the algorithm used to estimate the size of children that are not actually visible.
+  ///
+  /// [keyboardDismissBehavior] Creates a scrollable, linear array of widgets with a custom child model. For example, a custom child model can
+  /// control the algorithm used to estimate the size of children that are not actually visible.
+  ///
+  /// [restorationId] Creates a scrollable, linear array of widgets with a custom child model. For example, a custom child model can
+  /// control the algorithm used to estimate the size of children that are not actually visible.
+  ///
+  /// [clipBehavior] Creates a scrollable, linear array of widgets with a custom child model. For example, a custom child model can
+  /// control the algorithm used to estimate the size of children that are not actually visible.
+  ///
+  /// [itemBuilder] Conversation list item builder, return a widget if you need to customize it.
+  ///
+  /// [avatarBuilder] Avatar widget builder.
+  ///
+  /// [nicknameBuilder] Nickname widget builder.
+  ///
   const AgoraConversationsView({
     super.key,
-    this.onItemTap,
     this.controller,
+    this.onItemTap,
     this.reverse = false,
     this.primary,
     this.physics,
@@ -135,23 +189,61 @@ class AgoraConversationsView extends StatefulWidget {
     this.nicknameBuilder,
   });
 
+  /// The ScrollController for the conversation list.
   final ScrollController? controller;
+
+  /// Conversation list item builder, return a widget if you need to customize it.
   final AgoraConversationItemWidgetBuilder? itemBuilder;
+
+  /// Avatar widget builder.
   final AgoraConversationWidgetBuilder? avatarBuilder;
+
+  /// Nickname widget builder.
   final AgoraConversationWidgetBuilder? nicknameBuilder;
 
+  /// Creates a scrollable, linear array of widgets with a custom child model. For example, a custom child model
+  /// can control the algorithm used to estimate the size of children that are not actually visible.
   final bool reverse;
+
+  /// Creates a scrollable, linear array of widgets with a custom child model. For example, a custom child model can
+  /// control the algorithm used to estimate the size of children that are not actually visible.
   final bool? primary;
+
+  /// Creates a scrollable, linear array of widgets with a custom child model.
+  /// For example, a custom child model can control the algorithm used to estimate the size of children that are not actually visible.
   final ScrollPhysics? physics;
+
+  /// Creates a scrollable, linear array of widgets with a custom child model. For example, a custom child model
+  /// can control the algorithm used to estimate the size of children that are not actually visible.
   final bool shrinkWrap;
+
+  /// Creates a scrollable, linear array of widgets with a custom child model. For example, a custom child model
+  /// can control the algorithm used to estimate the size of children that are not actually visible.
   final EdgeInsetsGeometry? padding;
 
+  /// Creates a scrollable, linear array of widgets with a custom child model. For example, a custom child model
+  /// can control the algorithm used to estimate the size of children that are not actually visible.
   final double? cacheExtent;
+
+  /// Creates a scrollable, linear array of widgets with a custom child model.For example, a custom child model can
+  /// control the algorithm used to estimate the size of children that are not actually visible.
   final DragStartBehavior dragStartBehavior;
+
+  /// Creates a scrollable, linear array of widgets with a custom child model. For example, a custom child model can
+  /// control the algorithm used to estimate the size of children that are not actually visible.
   final ScrollViewKeyboardDismissBehavior keyboardDismissBehavior;
+
+  /// Creates a scrollable, linear array of widgets with a custom child model. For example, a custom child model can
+  /// control the algorithm used to estimate the size of children that are not actually visible.
   final String? restorationId;
+
+  /// Creates a scrollable, linear array of widgets with a custom child model. For example, a custom child model can
+  /// control the algorithm used to estimate the size of children that are not actually visible.
   final Clip clipBehavior;
+
+  /// Conversation list item Click event callback.
   final void Function(ChatConversation conversation)? onItemTap;
+
   @override
   State<AgoraConversationsView> createState() => AgoraConversationsViewState();
 
