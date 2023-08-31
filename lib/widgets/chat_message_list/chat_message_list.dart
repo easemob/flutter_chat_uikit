31d1eb54b13e56fa4d09eba5fcfff32820df9b4c
dujiepeng <347302029@qq.com>
2023-08-31 16:56:03 +0800

    call markAllMessagesAsRead will msgList pop.

--- lib/widgets/chat_message_list/chat_message_list.dart
+++ lib/widgets/chat_message_list/chat_message_list.dart
@@ -561,6 +561,7 @@ class _ChatMessagesListState extends State<ChatMessagesList>
 
     content = WillPopScope(
       onWillPop: () async {
+        widget.conversation.markAllMessagesAsRead();
         ChatUIKit.of(context).conversationsController?.loadAllConversations();
         return true;
       },
